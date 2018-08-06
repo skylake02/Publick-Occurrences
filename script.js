1138212d35c154be5dd9df98f6faa4567ad11ba6
Skylake02 <mattngaw02@gmail.com>
2018-08-06 19:40:48 -0400

    added AJAX functionality

--- script.js
+++ script.js
@@ -4,6 +4,21 @@
 $(function () {
     'use strict';
     
+    var content     = $('main .wrapper'),
+        a1          = $('#a1'),
+        a2          = $('#a2'),
+        a3          = $('#a3');
     
+    content.load('latest.html');
+    
+    a1.on('click', function () {
+        content.load('latest.html');
+    });
+    a2.on('click', function () {
+        content.load('archive.html');
+    });
+    a3.on('click', function () {
+        content.load('about.html');
+    });
     
 });
