5cc39944c9c3feb84e632017879c3b0b2ebab2a0
Skylake02 <mattngaw02@gmail.com>
2018-08-11 21:31:35 -0400

    added a shit ton of loading animations

--- script.js
+++ script.js
@@ -4,10 +4,22 @@
 $(function () {
     'use strict';
     
-    var content     = $('main .wrapper'),
+    var body        = $('body'),
+        load        = $('#load'),
+        hidden      = $('#hidden'),
+        content     = $('main .content'),
         a1          = $('#a1'),
         a2          = $('#a2'),
         a3          = $('#a3');
+    
+    window.setInterval(function () {
+        load.addClass('fadeOut');
+        window.setInterval(function () {
+            load.remove();
+            body.removeClass('stop-scrolling');
+            hidden.removeClass('hidden');
+        }, 500);
+    }, 3000);
     
     content.load('latest.html');
     
